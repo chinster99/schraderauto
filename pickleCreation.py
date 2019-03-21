ce9df9c9919c5379d3b2f90937891bf55e851c16
jflom <jflom@umich.edu>
2019-03-21 14:00:01 -0400

    Update pickleCreation.py

--- pickleCreation.py
+++ pickleCreation.py
@@ -3,5 +3,4 @@ hashmap = {
            83260060 : (Nik, 0)
            34082872 : (Jack, 0)
         }
-pickle.dump(hashmap, open("hashdoc.txt", "wb"))  
-           
+pickle.dump(hashmap, open("hashdoc.txt", "wb"))
